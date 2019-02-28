49531c641e3f672354b9edd336f66434b3b299c9
Golnaz Valizade <golnaz.valizade@gmail.com>
2019-02-28 18:39:36 +0330

    add project

---
+++ asset/js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $(".menu-toggle, .fa-times").on("click", function () {
+
+        $("nav").toggleClass("active");
+        $(".overlay").toggleClass("menu-open");
+
+    });
+
+    $(".overlay").on("click", function () {
+
+        $("nav").removeClass("active");
+        $(this).removeClass("menu-open");
+
+    });
+
+});
